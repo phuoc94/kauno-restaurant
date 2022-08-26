ef68598177d6bc80ce7ecc7f32dc8d7fa2046ddd
Phuoc Nguyen <phuoc94@hotmail.fi>
2022-08-26 10:18:30 +0300

    add SSRProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css';
 import '../styles/overwrite.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { SSRProvider } from 'react-bootstrap';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  );
 }
 
 export default MyApp;
