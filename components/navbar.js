0b5eb2945fc1cb22c6ef48761704cadcdad0cd98
Phuoc Nguyen <phuoc94@hotmail.fi>
2022-11-18 12:40:39 +0200

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -10,31 +10,36 @@ import Link from 'next/link';
 const CustomNavbar = () => {
   return (
     <>
-      <Navbar expand="lg">
-        <Container>
-          <Link href="/" passHref>
-            <Navbar.Brand className="row col-8 col-md-5 col-lg-3">
-              <Image src={Logo} alt="logo" />
-            </Navbar.Brand>
-          </Link>
+      <Navbar expand="xl">
+        <Container className="d-flex">
+          <Navbar.Brand as={Link} href="/">
+            <Image src={Logo} alt="logo" width={324} height={92} />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="justify-content-center w-100 h3 font-300 color-primary text-center">
-              <Link href="/" passHref>
-                <Nav.Link className="mx-1">Etusivu</Nav.Link>
-              </Link>
-              <Link href="/menu" passHref>
-                <Nav.Link className="mx-1">Menu</Nav.Link>
-              </Link>
-              <Link href="/about-us" passHref>
-                <Nav.Link className="mx-1">Meistä</Nav.Link>
-              </Link>
-              <Link href="/contact-us" passHref>
-                <Nav.Link className="mx-1 d-lg-none">Ota yhteyttä</Nav.Link>
-              </Link>
-              <Link href="/contact-us#reservation" passHref>
-                <Nav.Link className="mx-1 d-lg-none">Varaa pöytä</Nav.Link>
-              </Link>
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-center"
+          >
+            <Nav className="h3 font-300 color-primary text-center">
+              <Nav.Link as={Link} href="/" className="mx-1">
+                Etusivu
+              </Nav.Link>
+              <Nav.Link as={Link} href="/menu" className="mx-1">
+                Menu
+              </Nav.Link>
+              <Nav.Link as={Link} href="/about-us" className="mx-1">
+                Meistä
+              </Nav.Link>
+              <Nav.Link as={Link} href="/contact-us" className="mx-1 d-xl-none">
+                Ota yhteyttä
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                href="/contact-us#reservation"
+                className="mx-1 d-xl-none"
+              >
+                Varaa pöytä
+              </Nav.Link>
               {/*
               <NavDropdown title="EN" id="basic-nav-dropdown" className="mx-1">
                 <NavDropdown.Item href="#action/3.2">
@@ -46,21 +51,19 @@ const CustomNavbar = () => {
               </NavDropdown>
   */}
             </Nav>
-            <div className="col-lg-3 col-12 text-center d-none d-lg-block">
-              <Link href="/contact-us">
-                <button className="button-secondary text-uppercase text-nowrap py-3 p-2 m-1 button">
-                  <span className="h4 font-300 text-center">Ota yhteyttä</span>
-                </button>
-              </Link>
-            </div>
-            <div className="col-lg-3 col-12 text-center d-none d-lg-block">
-              <Link href="/contact-us#reservation">
-                <button className="button-primary text-uppercase text-nowrap py-3 p-2 m-1 button">
-                  <span className="h4 font-300 text-center">Varaa pöytä</span>
-                </button>
-              </Link>
-            </div>
           </Navbar.Collapse>
+          <div className="text-center d-none d-xl-block">
+            <Link href="/contact-us">
+              <button className="button-secondary text-uppercase text-nowrap py-3 p-2 m-1 button">
+                <span className="h4 font-300 text-center">Ota yhteyttä</span>
+              </button>
+            </Link>
+            <Link href="/contact-us#reservation">
+              <button className="button-primary text-uppercase text-nowrap py-3 p-2 m-1 button">
+                <span className="h4 font-300 text-center">Varaa pöytä</span>
+              </button>
+            </Link>
+          </div>
         </Container>
       </Navbar>
     </>
