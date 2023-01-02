fee5abb76534005db21bebbe29bcb5b06633370b
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-01-02 20:14:49 +0200

    fix order button clicking area

--- components/FloatFooterBar.js
+++ components/FloatFooterBar.js
@@ -7,14 +7,12 @@ const FloatFooterBar = () => {
     <div className="float-footer-bar w-100">
       <div className="py-3 bg-culture">
         <Container className="d-flex justify-content-end">
-          <button className="button-primary text-uppercase text-nowrap">
-            <span
-              className="button"
-              data-glf-cuid="1f9b655f-fd45-4b5d-9cb4-30d6d24fb65d"
-              data-glf-ruid="bd207f57-4862-4324-9bd5-b695ce16c038"
-            >
-              Tilaa
-            </span>
+          <button
+            className="button-primary text-uppercase text-nowrap"
+            data-glf-cuid="1f9b655f-fd45-4b5d-9cb4-30d6d24fb65d"
+            data-glf-ruid="bd207f57-4862-4324-9bd5-b695ce16c038"
+          >
+            <span className="button">Tilaa</span>
           </button>
         </Container>
       </div>
