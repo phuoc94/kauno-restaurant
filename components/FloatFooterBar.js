5c046d7cbeed1aaf08709720d2bb1f5d78f78885
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-01-02 20:38:00 +0200

    update float-footer-bar

--- components/FloatFooterBar.js
+++ components/FloatFooterBar.js
@@ -6,7 +6,8 @@ const FloatFooterBar = () => {
   return (
     <div className="float-footer-bar w-100">
       <div className="py-3 bg-culture">
-        <Container className="d-flex justify-content-end">
+        <Container className="d-flex justify-content-between align-items-center">
+          <h2 className="color-d-sienna m-0">Varaa etukäteen</h2>
           <button
             className="button-primary text-uppercase text-nowrap"
             data-glf-cuid="1f9b655f-fd45-4b5d-9cb4-30d6d24fb65d"
