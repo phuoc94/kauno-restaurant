3cc0c8a4dc94ded77455572733a584f23e6900a2
Phuoc Nguyen <phuoc94@hotmail.fi>
2025-01-30 20:09:23 +0200

    Remove heading from Quandoo component for cleaner presentation

--- components/quandoo.js
+++ components/quandoo.js
@@ -3,8 +3,6 @@ import Script from 'next/script';
 export default function Quandoo() {
     return (
         <section style={{ marginTop: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <h2>Varaa pöytä Kauno Kitcheniin</h2>
-
             <div id="quandoo-booking-widget"></div>
             
             <Script
